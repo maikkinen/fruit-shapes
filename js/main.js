2940d7f2012f03d9ea2b9536d7bfd0af3e849f54
unknown <anna-maija.rauramaa@aalto.fi>
2020-10-27 07:34:07 +0200

    Prepare dom and js for animation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ window.addEventListener('DOMContentLoaded', function (e) {
   document.querySelector('hp-presentation').onclick = handlePresentationClick;
 });
 
+
+/* The nextElementSibling property returns the element
+ * immediately following the specified element, in the same tree level. */
+
 function handlePresentationClick(e) {
-  alert('clicked');
+  var current = document.querySelector('hp-slide.active');
+  var next = current.nextElementSibling;
+
+  /* Manipulating the DOM directly is quite brave.
+   * It doesn't hurt to assure that you switch to
+   * the righ element :)
+   * (According to W. Scott Means, especially larger companies are pecky about this.)
+   */
+  while(next && next.tagName != 'HP-SLIDE') {
+    next = next.nextElementSibling;
+  }
+
+  if(next) {
+    current.classList.remove('active');
+    next.classList.add('active');
+  }
 }
