632cb1471d7b3638fc46f6ecedcc8a285efd000a
unknown <anna-maija.rauramaa@aalto.fi>
2021-01-15 12:58:36 +0100

    fruits intro animation and bouncyness

--- js/main.js
+++ js/main.js
@@ -1,124 +1,106 @@
-window.addEventListener('DOMContentLoaded', function (e) {
-  var presentation = document.querySelector('hp-presentation');
+// Introducing the fruits
 
-  presentation.onclick = handlePresentationClick;
-  presentation.addEventListener('animationend', handleAnimationEnd, false);
-});
+gsap.from("h2", {
+  duration: 1.5,
+  opacity: 0,
+  // y: () => -50,
+  // ease: 'bounce.out',
+  scaleY: 0.55,
+  scaleX: 0.65,
+})
 
-/* The nextElementSibling property returns the element
- * immediately following the specified element, in the same tree level. */
+gsap.from(".fruit", {
+  duration: 2,
+  delay: 0.75,
+  opacity: 0,
+  y: () => Math.random() * -300,
+  stagger: 0.25,
+  ease: 'bounce.out',
 
-function handlePresentationClick(e) {
-  var current = document.querySelector('hp-slide.active');
-  var next = current.nextElementSibling;
+  scaleY: 0.75,
+  scaleX: 0.85,
+})
+;
 
-  /* Manipulating the DOM directly is quite brave.
-   * It doesn't hurt to assure that you switch to
-   * the righ element :)
-   * (According to W. Scott Means, especially larger companies are pecky about this.) 
-   */
-  while (next && next.tagName != 'HP-SLIDE') { //yes, all-caps is the correct format for the DOM
-    next = next.nextElementSibling;
-  }
+// constants
+const defaultOpacity = 0.5;
 
-  if (next) {
-    current.classList.remove('active');
-    next.classList.add('active');
+const kiwi = document.getElementById("kiwi")
+const starfruit = document.getElementById("starfruit")
+const pitahaya = document.getElementById("pitahaya")
+const orange = document.getElementById("orange")
 
-    next.querySelectorAll('.match').forEach(function (el) {
-      setTimeout(function () { el.classList.remove('match'); }, 0);
-    });
+// create a new timeline in a paused state
+var tl = new TimelineMax({ paused: true });
 
-    var aa = parseInt(next.getAttribute('data-autoadvance'));
+kiwi.addEventListener("mouseover", () => squeeze('kiwi'));
+starfruit.addEventListener("mouseover", () => squeeze('starfruit'));
+pitahaya.addEventListener("mouseover", () => squeeze('pitahaya'));
+orange.addEventListener("mouseover", () => squeeze('orange'));
 
-    if (!isNaN(aa)) {
-      setTimeout(function (e) {
-        handlePresentationClick(e);
-      }, aa);
-    }
+kiwi.addEventListener("click", () => shake('kiwi'));
+starfruit.addEventListener("click", () => shake('starfruit'));
+pitahaya.addEventListener("click", () => shake('pitahaya'));
+orange.addEventListener("click", () => shake('orange'));
 
-    // getAttribute refers to DOM elements' attributes.
-    // osa stands for on show animation
-    var osa = next.getAttribute('data-onshow');
-    if (osa) {
-      window[osa]();
-    }
-  }
+const spin = () => {
+  TweenMax.set("#starfruit", {
+    transformOrigin: "50% 50%",        // make transform origin be center for x and y axis
+    opacity: 1,
+  });
+  // scale element to 100%
+  TweenMax.to("#starfruit", 2, {
+    rotation: "+=360",
+    ease: 'bounce',
+    opacity: defaultOpacity,
+  })
 }
 
-function handleAnimationEnd(e) {
-  var slide = e.target.closest('hp-slide');
-  /* 'closest' is a function that walks up the parent chain in the DOM 
-   * and looks for the closest parent element that has the tag
-   * that matches to the function's parameter.
-   */
-  var aa = slide.getAttribute('data-autoadvance');
+const bounce = (fruitName) => {
+  // set the initial scale and rotation to 0
+  // and the transform-origin 50% 50%
+  TweenMax.set(`#${fruitName}`, {
+    transformOrigin: "50% 50%",        // make transform origin be center for x and y axis
+    opacity: 1,
+  });
 
-  if (aa == 'animationend' && slide.classList.contains('active')) {
-    handlePresentationClick(e);
-  }
+  TweenMax.from(`#${fruitName}`, 0.4, {
+    scaleY: 0.75,
+    scaleX: 0.85,
+  })
+  TweenMax.to(`#${fruitName}`, 1.5, {
+    scaleY: 1,
+    scaleX: 1,
+    opacity: defaultOpacity,
+  })
+
+    .play();
 }
 
-function setLearnImage(imageName) {
-   //goes down the element tree and looks for the param
-  var img = document.querySelector('hp-slide.active hp-learn img');
+const shake = (fruitName) => {
+  TweenMax.set(`#${fruitName}`, {
+    transformOrigin: "50% 50%",        // make transform origin be center for x and y axis
+  });
 
-  if (img) { //changes the img element's src image :) 
-    img.src = 'images/' + imageName + '.svg';
-  }
+  TweenMax.from(`#${fruitName}`, 0.1, {
+    y: -30,
+    yoyo: true,
+    repeat: 6,
+    scale: 1.15,
+  })
 }
 
-var shapes = ['Starfruit', 'Kiwi', 'Pitahaya', 'Orange'];
+const squeeze = (fruitName) => {
+  TweenMax.set(`#${fruitName}`, {
+    transformOrigin: "50% 50%",        // make transform origin be center for x and y axis
+    opacity: 1,
+  });
 
-function showLearning() {
-  var ii = Math.floor(Math.random() * shapes.length);
-  setLearnImage(shapes[ii]);
-
-  var slide = document.querySelector('hp-slide.active');
-  slide.classList.remove('learn-yes');
-  slide.classList.remove('learn-no');
-  slide.classList.add(ii ? 'learn-no' : 'learn-yes'); //zero is 'falsy'!
-  //By adding classNames to the classlist, we can dynamically change
-  //what css rules apply to the element.
-}
-
-function startLearning(learningDelay) {
-  showLearning();
-
-  setTimeout(function () {
-    if (learningDelay > 1.1) {
-      showLearning();
-
-      learningDelay = Math.pow(learningDelay, 1 / 1.05);
-      startLearning(learningDelay);
-    }
-  }, learningDelay);
-}
-
-function runLearningSequence() {
-  startLearning(1500);
-}
-
-function animateSVGStep() {
-  var slide = document.querySelector('hp-slide.active');
-
-  var svgs = slide.querySelectorAll('svg');
-
-  if (svgs[0].children.length > 0) {
-    var el = svgs[0].children[0];
-
-    if (el) {
-      svgs[1].appendChild(el.parentNode.removeChild(el));
-    }
-
-    return true;
-  }
-
-  return false;
-}
-
-function animateSVG() {
-  if (animateSVGStep()) {
-    setTimeout(animateSVG, 400); //tweak timeout val (ms)
-  }
+  TweenMax.from(`#${fruitName}`, 0.25, {
+    scale: 1.1
+  })
+  TweenMax.to(`#${fruitName}`, 0.5, {
+    rotation: "+=360",
+    scale: 1
+  })
 }
